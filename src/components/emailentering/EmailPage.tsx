8edce2c0f32d248208325a731c2cea5cc74a7942
Your Name <you@example.com>
2022-01-06 21:45:03 +0200

    Navigation

--- src/components/emailentering/EmailPage.tsx
+++ src/components/emailentering/EmailPage.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Footer from "../footer/Footer";
-
 import './EmailPage.css';
 import Frame from "../frame/Frame";
+// @ts-ignore
+import { useHistory } from "react-router-dom";
+
 function EmailPage(): JSX.Element {
+    const history = useHistory();
     function saveEmail(e: React.MouseEvent<HTMLElement>) {
         e.preventDefault();
-        console.log('You clicked submit.');
+        history.push("/payment");
     }
-
     return (
         <Frame>
             <div>
