97d0991511dcce0131995af334773c284309b525
Your Name <you@example.com>
2022-01-23 17:15:37 +0200

    Busy

--- src/components/instruments/Instruments.js
+++ src/components/instruments/Instruments.js
@@ -3,12 +3,15 @@ import Frame from "../frame/Frame";
 import {appdata, load, save} from "../AppData";
 import {CountryDropdown} from "react-country-region-selector";
 import {useState} from "react";
+import {Paysera} from "./paysera/paysera";
+import payseraData from "../instrumentsOld/paysera.json";
 
 function useForceUpdate(){
     const [value, setValue] = useState(0); // integer state
     return () => setValue(value => value + 1); // update the state to force render
 }
-function Instruments() {
+
+export function Instruments() {
     const forceUpdate = useForceUpdate();
     const options = {
         clientSecret: 'pi_3KFlMXKNGtOWS9rw0kYh6Ni0_secret_a3rKNOqwSXc8qrG2Co9TikQQX'
@@ -22,33 +25,21 @@ function Instruments() {
     return (
         <div>
             <Frame>
-                <h2 className="offer-pay__title">
-                    Выберете способ оплаты
-                </h2>
-                <div className="App">
-                    AppDataCountry{appdata.country}---
-                </div>
+                <br/>
                 <CountryDropdown
                     valueType = "short"
                     value={appdata.country}
                     onChange={(val) => selectCountry(val)} />
-
-                <br/>
-                <ul className="offer-pay__list">
-                    <li className="offer-pay__item">
-                        <div id="paypal-button-container" className="paypal"/>
-                    </li>
-                </ul>
-                <div id="smart-button-container">
-                    <div className="stripe">
-                    </div>
-                    <div className="paypal">
-                    </div>
+                <h2 className="offer-pay__title">
+                    Выберете способ оплаты
+                </h2>
+                <div className="App">
                 </div>
+                <br/>
+                <Paysera></Paysera>
             </Frame>
         </div>
     );
 }
-
 export default Instruments;
 
---
+++ src/components/instruments/paysera/paysera.js
@@ -0,0 +1,29 @@
+import payseraData from "../../instrumentsOld/paysera.json";
+import {appdata, save} from "../../AppData";
+import axios from "axios";
+
+export function Paysera() {
+    const paysera = (method) => {
+        console.log(method)
+        axios.post('https://api.duoclassico.eu/functions/init', appdata)
+            .then(response => {
+                // @ts-ignore
+                appdata.country = response.data["country"];
+                window.open("https://www.google.com","_self");
+        })
+    }
+    return <div>
+        <div className="App">
+            <ul className="offer-pay__banks">
+                {
+                    (payseraData["data"][appdata.country.toLowerCase()] !== undefined)?
+                    Object.keys(payseraData["data"][appdata.country.toLowerCase()]).map(function(name, index){
+                        return <li key={ index }>
+                                <img src={payseraData["data"][appdata.country.toLowerCase()][name]["url"]} alt="" onClick={()=>paysera(name)}></img>
+                        </li>
+                    }):<div>Paysera</div>
+                }
+            </ul>
+        </div>
+    </div>
+}
