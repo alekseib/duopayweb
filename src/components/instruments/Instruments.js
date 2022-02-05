64ca302ced6291f02f32c060ff285cbad092fce2
Your Name <you@example.com>
2022-02-05 15:11:20 +0200

    Instruments refactoring

--- src/components/instruments/Instruments.js
+++ src/components/instruments/Instruments.js
@@ -5,7 +5,6 @@ import {CountryDropdown} from "react-country-region-selector";
 import React, {useState} from "react";
 import {useHistory} from "react-router-dom";
 import axios from "axios";
-import {Spinner} from "react-bootstrap";
 import payseraData from "../providers/paysera/paysera.json";
 import montonioData from "../providers/montonio/montonio.json";
 import ReactPixel from "react-facebook-pixel";
@@ -16,13 +15,8 @@ function useForceUpdate() {
     const [value, setValue] = useState(0); // integer state
     return () => setValue(value => value + 1); // update the state to force render
 }
-
-let busy1 = false
-let busy2 = false
-let busy3 = false
 let errorMessage = "";
-let busyMontonio = false;
-let busyPaysera = false;
+let busy = "";
 
 
 export function Instruments() {
@@ -42,139 +36,15 @@ export function Instruments() {
         forceUpdate();
     }
 
-    function selectPayPal() {
+    function selectInstrument(paymentMethod,instrument,nextPage,spinnerKey) {
         errorMessage = "";
-        if (busy1) return;
-        busy1 = true;
+        if (busy === spinnerKey) return;
+        busy = spinnerKey;
         forceUpdate();
         ReactPixel.init('325830968618472');
         ReactPixel.track('InitiateCheckout');
 
-        appdata.paymentMethod = "PAYPAL";
-        appdata.paymentInstrument = "PayPal";
-        axios.post('https://api.duoclassico.eu/functions/init', appdata)
-            .then(response => {
-                if (response.status !== 200) {
-                    errorMessage = response.statusText;
-                    forceUpdate();
-                    return;
-                }
-                // @ts-ignore
-                appdata.orderId = response.data["orderId"];
-                save();
-                registerInitialization();
-                history.push("/paypal");
-            })
-            .catch((error) => {
-                errorMessage = error.message;
-                return Promise.reject(error)
-            })
-            .finally(() => {
-                busy1 = false;
-                forceUpdate();
-            });
-    }
-
-    function selectStripe() {
-        errorMessage = "";
-        if (busy2) return;
-        busy2 = true;
-        forceUpdate();
-        appdata.paymentMethod = "STRIPE";
-        appdata.paymentInstrument = "card";
-        axios.post('https://api.duoclassico.eu/functions/init', appdata)
-            .then(response => {
-                if (response.status !== 200) {
-                    errorMessage = response.statusText;
-                    forceUpdate();
-                    return;
-                }
-                // @ts-ignore
-                appdata.orderId = response.data["orderId"];
-                appdata.redirectData = response.data["redirectData"];
-                save();
-                registerInitialization();
-                history.push("/stripe");
-            })
-            .catch((error) => {
-                errorMessage = error.message;
-                return Promise.reject(error)
-            })
-            .finally(() => {
-                busy2 = false;
-                forceUpdate();
-            });
-    }
-
-    function selectGoogleApple() {
-        errorMessage = "";
-        if (busy3) return;
-        busy3 = true;
-        forceUpdate();
-        appdata.paymentMethod = "STRIPE";
-        appdata.paymentInstrument = "card";
-        axios.post('https://api.duoclassico.eu/functions/init', appdata)
-            .then(response => {
-                if (response.status !== 200) {
-                    errorMessage = response.statusText;
-                    forceUpdate();
-                    return;
-                }
-                // @ts-ignore
-                appdata.orderId = response.data["orderId"];
-                appdata.redirectData = response.data["redirectData"];
-                save();
-                registerInitialization();
-                history.push("/stripe");
-            })
-            .catch((error) => {
-                errorMessage = error.message;
-                return Promise.reject(error)
-            })
-            .finally(() => {
-                busy3 = false;
-                forceUpdate();
-            });
-    }
-
-    function selectMontonio(bic) {
-        errorMessage = "";
-        if (busyMontonio) return;
-        busyMontonio = true;
-        forceUpdate();
-
-        appdata.paymentMethod = "MONTONIO";
-        appdata.paymentInstrument = bic;
-        axios.post('https://api.duoclassico.eu/functions/init', appdata)
-            .then(response => {
-                if (response.status !== 200) {
-                    errorMessage = response.statusText;
-                    forceUpdate();
-                    return;
-                }
-                // @ts-ignore
-                appdata.orderId = response.data["orderId"];
-                appdata.redirectData = response.data["redirectData"];
-                save();
-                registerInitialization();
-                window.open(appdata.redirectData, "_self");
-            })
-            .catch((error) => {
-                errorMessage = error.message;
-                return Promise.reject(error)
-            })
-            .finally(() => {
-                busyMontonio = false;
-                forceUpdate();
-            });
-    }
-
-    function selectPaysera(instrument) {
-        errorMessage = "";
-        if (busyPaysera) return;
-        busyPaysera = true;
-        forceUpdate();
-        appdata.paymentMethod = "PAYSERA";
+        appdata.paymentMethod = paymentMethod;
         appdata.paymentInstrument = instrument;
         axios.post('https://api.duoclassico.eu/functions/init', appdata)
             .then(response => {
@@ -188,18 +58,23 @@ export function Instruments() {
                 appdata.redirectData = response.data["redirectData"];
                 save();
                 registerInitialization();
-                window.open(appdata.redirectData, "_self");
+                if (nextPage === "REDIRECT")
+                {
+                    window.open(appdata.redirectData, "_self");
+                }
+                else {
+                    history.push(nextPage);
+                }
             })
             .catch((error) => {
                 errorMessage = error.message;
                 return Promise.reject(error)
             })
             .finally(() => {
-                busyPaysera = false;
+                busy = "";
                 forceUpdate();
             });
     }
-
     return (
         <div>
             <Frame>
@@ -217,8 +92,8 @@ export function Instruments() {
                 <p className="app-text">
                     {errorMessage}
                 </p>
-                {appdata.customerEmail === "paysera@duoclassico.eu" ?
-                    (busyPaysera) ? <MySpinner></MySpinner> :
+                {appdata.customerEmail === "paysera@duoclassico.eu" || !"EE,LV,LT".includes(appdata.country) ?
+                    (busy === "PAYSERA") ? <MySpinner></MySpinner> :
                         <div>
                             <div className="App">
                                 <ul className="offer-pay__banks">
@@ -228,7 +103,7 @@ export function Instruments() {
                                                 return <li key={index}>
                                                     <img
                                                         src={payseraData["data"][appdata.country.toLowerCase()][name]["url"]}
-                                                        alt="" onClick={() => selectPaysera(name)}
+                                                        alt="" onClick={() => selectInstrument("PAYSERA",name,"REDIRECT", "PAYSERA")}
                                                         className="bank-logo"/>
                                                 </li>
                                             }) : <div></div>
@@ -237,7 +112,7 @@ export function Instruments() {
                             </div>
                         </div>
                     :
-                    busyMontonio ? <MySpinner></MySpinner> :
+                    busy === "MONTONIO"? <MySpinner></MySpinner> :
                         <div className="App">
                             <ul className="offer-pay__banks">
                                 {
@@ -245,7 +120,7 @@ export function Instruments() {
                                     montonioData[appdata.country].map((data) => {
                                         return <li key={data.bic}>
                                             <img src={data.logo_url} className="bank-logo"
-                                                 alt="" onClick={() => selectMontonio(data.bic)}/>
+                                                 alt="" onClick={() => selectInstrument("MONTONIO",data.bic,"REDIRECT", "MONTONIO")}/>
                                         </li>
                                     }) : <div></div>
                                 }
@@ -253,18 +128,18 @@ export function Instruments() {
                         </div>
                 }
                 <div className="app-button d-flex justify-content-around">
-                    <button onClick={() => selectStripe()} className="app-btn1">
-                        {busy2 ? <MySpinner/> : <span><img src="img/Cards.png" alt=""/></span>}
+                    <button onClick={() => selectInstrument("STRIPE", "card", "/stripe", "STRIPE")} className="app-btn1">
+                        {busy === "STRIPE"? <MySpinner/> : <span><img src="img/Cards.png" alt=""/></span>}
                     </button>
                 </div>
                 <div className="app-button d-flex justify-content-around">
-                    <button onClick={() => selectPayPal()} className="app-btn2">
-                        {busy1 ? <MySpinner/> : <span><img src="img/PayPal.png" alt=""/></span>}
+                    <button onClick={() => selectInstrument("PAYPAL","Paypal","/paypal", "PAYPAL")} className="app-btn2">
+                        {busy === "PAYPAL"? <MySpinner/> : <span><img src="img/PayPal.png" alt=""/></span>}
                     </button>
                 </div>
                 <div className="app-button d-flex justify-content-around">
-                    <button onClick={() => selectGoogleApple()} className="app-btn3">
-                        {busy3 ? <MySpinner/> : <span><img src="img/GoogleApple.png" alt=""/></span>}
+                    <button onClick={() => selectInstrument("STRIPE", "card", "/stripe", "GOOGLE")} className="app-btn3">
+                        {busy === "GOOGLE"? <MySpinner/> : <span><img src="img/GoogleApple.png" alt=""/></span>}
                     </button>
                 </div>
 
