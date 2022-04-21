69f4c410b211b8388b4a86a898bfdc036dd4df01
Your Name <you@example.com>
2022-04-21 19:30:23 +0300

    Phone OK

--- src/components/instruments/CashOKPage.js
+++ src/components/instruments/CashOKPage.js
@@ -1,12 +1,6 @@
 import Frame from "../common/Frame";
 import React, {useState} from "react";
 import Footer from "../common/Footer";
-import {appdata, save} from "../model/AppData";
-import ReactPixel from "react-facebook-pixel";
-import {MySpinner} from "../common/MySpinner";
-import axios from "axios";
-import {useHistory} from "react-router-dom";
-
 
 export function CashOKPage() {
     // @ts-ignore
