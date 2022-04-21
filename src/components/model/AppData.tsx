0ab31bb1fe2471165e229ecb4af5981bb2c236e7
Your Name <you@example.com>
2022-04-21 13:19:34 +0300

    CashIcon

--- src/components/model/AppData.tsx
+++ src/components/model/AppData.tsx
@@ -48,6 +48,7 @@ export function load()
         appdata["country"] = state1["country"];
         appdata["description"] = state1["description"];
         appdata["redirectData"] = state1["redirectData"];
+        appdata["eventType"] = state1["eventType"];
 
     }
     catch (e)
